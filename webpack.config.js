5a9f4ab72cfac0a3c43777b5e8777f9210ad9074
Zhao Yongzhen <icstorm2020@outlook.com>
2022-07-13 19:54:24 +0800

    🎉 init: framework init

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.ts",
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: "gray.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,    // .ts或者tsx后缀的文件，就是typescript文件
+                use: "ts-loader",   // 就是上面安装的ts-loader
+                exclude: "/node-modules/" // 排除node-modules目录
+            }
+        ]
+    },
+    // 模式
+    mode: "development",
+}
